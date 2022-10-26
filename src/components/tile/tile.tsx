2b4e96d5d6bbf23efe15ddbd24724f902c6d96fb
Leon <leon.chow1@ontariotechu.net>
2022-10-26 01:06:27 -0400

    Remove card name in tiles

--- src/components/tile/tile.tsx
+++ src/components/tile/tile.tsx
@@ -9,7 +9,6 @@ const Tile = (props: { number: number; card: ICard | null }) => {
   return (
     <div className="tile" onClick={() => placeCard}>
       {props.number}
-      {props.card ? <div> {props.card.name}</div> : ""}
     </div>
   );
 };
